313404967685eae52bdfb5d43f3a4ed76d815b92
JihenAridhi <aridhijihen1@gmail.com>
2025-05-29 12:13:51 +0100

    initial interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { WalletMngComponent } from './components/wallet-mng/wallet-mng.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "admin", component: SidebarComponent, children:
+        [
+            {path: "wallets", component: WalletMngComponent}
+        ]
+    }
+];
---
+++ src/app/components/wallet-mng/wallet-mng.component.ts
@@ -0,0 +1,14 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as echarts from 'echarts';
+
+@Component({
+  selector: 'app-wallet-mng',
+  imports: [],
+  templateUrl: './wallet-mng.component.html',
+  styleUrl: './wallet-mng.component.css'
+})
+export class WalletMngComponent {
+
+  
+
+}
