bf248084eed6ce043c15d6e8320d263298323ef8
JihenAridhi <aridhijihen1@gmail.com>
2025-05-30 13:35:50 +0100

    accounting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { WalletMngComponent } from './components/wallet-mng/wallet-mng.component';
+import { AccountingComponent } from './components/accounting/accounting.component';
 
 export const routes: Routes = [
     {
         path: "admin", component: SidebarComponent, children:
         [
-            {path: "wallets", component: WalletMngComponent}
+            {path: "wallets", component: WalletMngComponent},
+            {path:"accounting", component: AccountingComponent}
         ]
     }
 ];
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  isCollapsed = false;
+
+  toggleSidebar(): void {
+    this.isCollapsed = !this.isCollapsed;
+    
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.getElementById('mainContent');
+    
+    if (sidebar && mainContent) {
+      sidebar.classList.toggle('collapsed');
+      mainContent.classList.toggle('expanded');
+      (mainContent as HTMLElement).style.marginLeft = this.isCollapsed ? '80px' : '260px';
+    }
+  }
+
+  // In component.ts
+  currentYear: number = new Date().getFullYear();
 
 }
