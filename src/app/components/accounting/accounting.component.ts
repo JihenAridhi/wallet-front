9a65b43df68a3e2faa6ec0b6bcbcc985e7f568d4
JihenAridhi <aridhijihen1@gmail.com>
2025-06-01 05:17:37 +0100

    accounting tabs

--- src/app/components/accounting/accounting.component.ts
+++ src/app/components/accounting/accounting.component.ts
@@ -9,17 +9,57 @@ import { Component } from '@angular/core';
 })
 export class AccountingComponent {
 
-  isNewSchemaVisible: boolean = false;
+    isNewSchemaVisible: boolean = false;
+    isNewOperationTypeVisible: boolean = false;
+    isNewFeeVisible: boolean = false;
+    isNewMappingVisible: boolean = false;
+    isNewVatVisible: boolean = false;
 
-  // Toggle the visibility of the wallet creation form
   toggleSchemaForm(): void {
-    console.log(this.isNewSchemaVisible)
     this.isNewSchemaVisible = true;
   }
-  // Optional: Method to close the form (e.g., when clicking "Cancel")
   closeSchemaForm(): void {
-    console.log(this.isNewSchemaVisible)
     this.isNewSchemaVisible = false;
+  }
+
+   toggleForm(modal: String): void {
+    switch (modal)
+    {
+      case 'schema': this.isNewSchemaVisible = true; break;
+      case 'operation': this.isNewOperationTypeVisible = true; break;
+      case 'fee': this.isNewFeeVisible = true; break;
+      case 'mapping': this.isNewMappingVisible = true; break;
+      case 'vat' : this.isNewVatVisible = true; break;
+    }
+  }
+  closeForm(modal: String): void {
+    switch (modal)
+    {
+      case 'schema': this.isNewSchemaVisible = false; break;
+      case 'operation': this.isNewOperationTypeVisible = false; break;
+      case 'fee': this.isNewFeeVisible = false; break;
+      case 'mapping': this.isNewMappingVisible = false; break;
+      case 'vat' : this.isNewVatVisible = false; break;
+    }
+  }
+
+  get isAnyModalVisible(): boolean {
+    return (
+      this.isNewSchemaVisible ||
+      this.isNewOperationTypeVisible ||
+      this.isNewFeeVisible ||
+      this.isNewMappingVisible ||
+      this.isNewVatVisible
+    );
+  }
+
+
+  toggleOpertionForm(): void {
+    console.log(this.isNewOperationTypeVisible)
+    this.isNewOperationTypeVisible = true;
+  }
+  closeOperationForm(): void {
+    this.isNewOperationTypeVisible = false;
   }
 
   showTab(tabId: string): void {
@@ -27,17 +67,17 @@ export class AccountingComponent {
     const tabContents = document.querySelectorAll('.tab-content');
 
     // Reset all buttons and contents
-    /* tabButtons.forEach(btn => {
+    tabButtons.forEach(btn => {
       btn.classList.remove('active', 'text-primary', 'font-medium');
       btn.classList.add('text-gray-500');
-    }); */
+    });
 
     tabContents.forEach(content => content.classList.add('hidden'));
 
     // Activate the clicked button and show its tab content
-   /*  const activeButton = document.getElementById(tabId);
+    const activeButton = document.getElementById(tabId);
     activeButton?.classList.add('active', 'text-primary', 'font-medium');
-    activeButton?.classList.remove('text-gray-500'); */
+    activeButton?.classList.remove('text-gray-500');
 
     const activeContent = document.getElementById(`tab-${tabId}`);
     activeContent?.classList.remove('hidden');
